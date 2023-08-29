32a723470d3b27e2fe943f623f32c9ca6c2eedfd
facundo abregu <132696469+facu703@users.noreply.github.com>
2023-08-29 16:14:46 -0300

    Delete loginfat directory

--- loginfat/vaciar.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id= $_POST["id"];
-$conexion= mysqli_connect("localhost","root","","travelmanatan") or exit ("no se puede conectar.....");
-$eliminar = "DELETE FROM travelmanatan1 WHERE id=$id";
-mysqli_query($conexion,$eliminar);
-
-?>
--- loginfat/vaciarfoto.php
+++
@@ -1,7 +0,0 @@
-<?php
-$id= $_POST["id"];
-$conexion= mysqli_connect("localhost","root","","travelmanatan") or exit ("no se puede conectar.....");
-$eliminar = "DELETE FROM fotostravel WHERE id_img=$id";
-mysqli_query($conexion,$eliminar);
-
-?>
